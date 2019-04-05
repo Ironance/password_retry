14cb71c7c58ec10bbcc80635b576a39f6dcb6a0e
Erika Wu <ironance@hotmail.com>
2019-04-05 15:03:39 +0800

    message modify

--- pw_guess.py
+++ pw_guess.py
@@ -2,15 +2,17 @@ login_pw = 'a123'
 trail = 3
 
 while trail > 0:
+    trail = trail - 1
     pw = input('Please input the password:')
     if pw == login_pw:
         print('Login Successfully!')
         break
     else:
-        trail = trail - 1
-        print('Wrong password!', trail, 'chance left')
-        if trail == 0:
-            print('Game Over!')
+        print('Wrong password!')
+        if trail > 0:
+            print(trail, 'chance left')
+        else:
+            print('Fail to login! Account is blocked!')
 
 
 
